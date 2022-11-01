cf97285f35ac6e66a83c7c28a274a1ac6b42ab8d
BhargavvarmaP <bhargav200794@gmail.com>
2022-11-01 22:47:38 +0530

    Web3

--- src/DecentralizedContentCreation.js
+++ src/DecentralizedContentCreation.js
@@ -1,20 +1,20 @@
-const {fs} = require("fs");
+const {fs, link} = require("fs");
 const {path} = require("path");
-const {IPFS} = require("ipfs-core");
+const {Ipfs} = require("ipfs-core");
 const name=document.getElementById("name").value;
 let URI;
-document.querySelector("#file").addEventListener("click",upload);
+document.querySelector("#file").addEventListener("change",upload);
 async function upload(){
        const filereader = new FileReader();
        filereader.readAsArrayBuffer(event.target.files[0]);
        filereader.onload=async (event)=>{
-        const node = await IPFS.create();
+        const node = await Ipfs.create();
         let {path} = await node.add(filereader.result);
         console.log(path);
         const ipfs="https://ipfs.io/ipfs/";
         URI=ipfs.concat(path);
         console.log(URI);
         console.log(name);
-        
+        document.getElementById("ipfslink").innerText=document.write(URI);
     }
 }
